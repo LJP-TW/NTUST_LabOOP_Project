377a14d72aa444e6697473f098855707623cbfc0
shungfu <39113361+shungfu@users.noreply.github.com>
2018-06-17 14:07:59 +0800

    command upload

--- Project3_SeaBattleSim/Project3_SeaBattleSim/CommonStruct.h
+++ Project3_SeaBattleSim/Project3_SeaBattleSim/CommonStruct.h
@@ -33,18 +33,38 @@ enum class VESSEL_ERROR
 enum class CMD_SET_ERROR
 {
 	TYPE_NOT_EXIST,
+	/*新增重複名稱錯誤*/
+	VESSEL_NAME_ERROR,
+	/*新增座標錯誤*/
+	COORDINATE_ERROR,
 };
 
 enum class CMD_FIRE_ERROR
 {
 	VESSEL_NOT_EXIST,
 	WEAPON_ILLEGAL,
+	/*新增座標錯誤*/
+	COORDINATE_ERROR,
 };
 
 enum class CMD_MOVE_ERROR
 {
 	VESSEL_NOT_EXIST,
 	SPEED_ILLEGAL,
+};
+
+/*新增防守錯誤*/
+enum class CMD_DEFENSE_ERROR
+{
+	VESSEL_NOT_EXIST,
+	WEAPON_ILLEGAL,
+	NOT_IN_RANGE,
+};
+
+enum class CMD_TAG_ERROR
+{
+	VESSEL_NOT_EXIST,
+	NEW_NAME_ILLEGAL,
 };
 
 // 所有艦種 定義在 Vessel.cpp
